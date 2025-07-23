47f5863554f075a9a94b5fda2145b8c6463a5c64
MohideenER <149752972+MohideenER@users.noreply.github.com>
2025-07-23 05:59:16 +0530

    Update Greeter.java
    Only open PR

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -3,7 +3,7 @@ package hello;
 public class Greeter {
     public String sayHello() {
 
-        return "Welcome to GitHub Demo .... Patch 1 updated third time";
+        return "Welcome to GitHub Demo .... Patch 1  .....";
 
     }
 }
