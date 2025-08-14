7d557287e0f1afa982de52d4b12df5c63e6b66de
MohideenER <149752972+MohideenER@users.noreply.github.com>
2025-08-14 08:47:47 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -3,7 +3,7 @@ package hello;
 public class Greeter {
     public String sayHello() {
 
-        return "Welcome to GitHub Demo .... Changed in code in branch and raised a pr.......";
+        return "Welcome to GitHub Demo .... Checking pull request open";
 
     }
 }
